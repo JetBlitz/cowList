53ac2db4761b9d6e09cb577fbf5a300e788e7def
Eivin Adlawan <eadlawa2@gmail.com>
2019-11-20 18:23:12 -0600

    Savepoint: created a schema.sql file, created a database called cowList, created a tables called cows, added rows into cows (cowId, cowName, cowDescription).

---
+++ server/db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS cowList;
+​
+USE cowList;
+​
+CREATE TABLE IF NOT EXISTS cows (
+  cowId INT AUTO_INCREMENT NOT NULL,
+  cowName VARCHAR(20) NOT NULL,
+  cowDescription VARCHAR(180) NOT NULL,
+  PRIMARY KEY (cowId),
+  UNIQUE (cowName)
+);
