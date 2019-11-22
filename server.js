b5adb483250b248b422b86dd2293b0b97d90f3f8
Eivin Adlawan <eadlawa2@gmail.com>
2019-11-21 19:28:24 -0600

    Savepoint: working on trying to organize my React files (components) and routes. All of the documentations are getting confusing to follow.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 const port = 3000;
 const db = require('./db/index.js')
 
+//! here are Routes that handle requests
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 // app.use(express.json()) //when receiving a JSON file
 app.use(express.urlencoded({ extended: true })) 
 // console.log('api/cows')
+
+
+//! Sends back all of my stored cows
 app.get('/api/cows', (req, res) => {
   // console.log('./api/cows')
   db.query('select * from cows', (err, data) => {
@@ -19,6 +28,7 @@ app.get('/api/cows', (req, res) => {
   })
 });
 
+//! inserts a new cow
 app.post('/api/cows', (req, res, next) => {
   console.log(req.body)
   db.query('INSERT INTO cows(cowName, cowDescription) values(?, ?)', ['Carl','Fat'], (err, data) => {
@@ -29,6 +39,26 @@ app.post('/api/cows', (req, res, next) => {
     }
   });
 
+
+//! Error handlers if everything above does not work
+// this is a custom 404 error handler
+
+app.use((req, res, next) => {
+  const error = new Error('Not found!');
+  error.status = 404;
+  next(error);
+});
+
+//! if something else breaks ANYWHERE in the app like the database
+app.use((error, req, res, next) => {
+  res.status(error.status || 500); //this will give me the problem's individual error status or a 500 status which is a databse
+  res.json({
+    error: {
+      message: error.message // this is the error message of the argument passed
+    }
+  })
+
+})
     // res.status(200).send(data);
 
   // res.status(201).json({
